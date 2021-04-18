b9444ea491c9cf4e163b1846087f027f137d9b8f
Marco Cusumano-Towner <imarcoam@gmail.com>
2021-04-18 16:15:19 -0400

    scaling online inference plot

--- examples/scaling/scaling.jl
+++ examples/scaling/scaling.jl
@@ -2,6 +2,7 @@ using Gen
 using Gen2DAgentMotion
 using PyPlot
 using Statistics: median
+using LaTeXStrings
 
 function evaluate_update_scaling(motion_model, path, params, observations, reps)
     elapsed = Matrix{Float64}(undef, length(observations), reps)
@@ -26,7 +27,8 @@ end
 function plots()
     path = Point[Point(0, 0), Point(1, 0), Point(1, 1)]
     params = ObsModelParams(0.05, 0.3, 1.0)
-    observations = Point[Point(i, i) for i in 1:500]
+    T = 1000
+    observations = Point[Point(i, i) for i in 1:T]
     reps = 5
     figure()
     pseudomarginal = evaluate_update_scaling(
@@ -44,13 +46,23 @@ function plots()
     collapsed_incremental = evaluate_update_scaling(
         motion_and_measurement_model_collapsed_incremental,
         path, params, observations, reps)
-    plot(pseudomarginal, label="pseudomarginal")
-    plot(uncollapsed, label="uncollapsed")
-    plot(uncollapsed_incremental, label="uncollapsed, incremental")
-    plot(collapsed, label="collapsed")
-    plot(collapsed_incremental, label="collapsed, incremental")
+    plot(log.(1:T), log.(pseudomarginal), label="pseudomarginal")
+    plot(log.(1:T), log.(uncollapsed), label="uncollapsed")
+    plot(log.(1:T), log.(uncollapsed_incremental), label="uncollapsed, incremental")
+    plot(log.(1:T), log.(collapsed), label="collapsed")
+    plot(log.(1:T), log.(collapsed_incremental), label="collapsed, incremental")
+    xlim = gca().get_xlim()
+    linear = collect(1:T)
+    quadratic = collect(1:T).^2
+    cubic = collect(1:T).^3
+    plot(log.(1:T), log.(linear) .- 10, color="black", linestyle="--")
+    plot(log.(1:T), log.(quadratic) .- 10, color="black", linestyle="--")
+    plot(log.(1:T), log.(cubic) .- 10, color="black", linestyle="--")
+    annotate(L"$O(T)$", (log(T), log(linear[end])))
+    annotate(L"$O(T^2)$", (log(T), log(quadratic[end])))
+    annotate(L"$O(T^3)$", (log(T), log(cubic[end])))
     legend()
-    gca().set_ylim((0, 1))
+    #gca().set_ylim((0, 1))
     savefig("scaling.png")
 end
 
